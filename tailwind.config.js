e6a4f56afa9fb3054ca5c27f7912513fb4f2208c
DrZiMo <zuhaybhamar@gmail.com>
2024-11-07 23:45:36 +0300

    finished the ideas section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
       colors: {
         green: "#3abab4",
         dark: "#1d1d20",
-        slight_white: "rgba(255, 255, 255, 0.07)"
+        light_dark: "#222225",
+        slight_white: "rgba(255, 255, 255, 0.07)",
+        slight_black: "rgba(0, 0, 0, 0.6)",
+        slight_green: "rgba(79, 209, 197, 0.3)"
       }
     },
   },
